5315295cc9c64d98f7d91f77ab7272457a651b18
David Bezuidenhout <tinuva@gmail.com>
2021-09-07 07:29:19 +0200

    rewrite smartthings to match other python3 plugins - untested

--- plugins/smartthings.py
+++ plugins/smartthings.py
@@ -1,47 +1,46 @@
-import httplib, urllib, json
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+"""Smartthings module"""
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+from tornado.httpclient import AsyncHTTPClient
 from tornado import gen
 
 from core import logger
-from core.config import config
-from core.events import events
+from core.events import Events
 
-def init():
-    config.SMARTTHINGS_ENABLE = config.read_config_var('smartthings', 'enable', False, 'bool')
-    if config.SMARTTHINGS_ENABLE == True:
-        config.SMARTTHINGS_ACCESS_TOKEN = config.read_config_var('smartthings', 'access_token', False, 'str')
-        config.SMARTTHINGS_URL_BASE = config.read_config_var('smartthings', 'url_base', False, 'str')
-        config.SMARTTHINGS_APP_ID = config.read_config_var('smartthings', 'app_id', False, 'str')
-        config.SMARTTHINGS_EVENT_CODES = config.read_config_var('smartthings', 'event_codes', [], 'listint')
-        logger.debug('SMARTTHINGS Enabled - event codes: %s' % (",".join([str (i) for i in config.SMARTTHINGS_EVENT_CODES])))
-        events.register('statechange', sendStNotification, [], [])
-        events.register('stateinit', sendStNotification, [], [])
+class Smartthings(object):
+    """Smartthings plugin class"""
+    def __init__(self, config):
+        """Init function for smartthings plugin"""
+        self.config = config
+        config.SMARTTHINGS_ENABLE = config.get_val('smartthings', 'enable', False, 'bool')
+        if config.SMARTTHINGS_ENABLE:
+            config.SMARTTHINGS_ACCESS_TOKEN = config.get_val('smartthings', 'access_token', False, 'str')
+            config.SMARTTHINGS_URL_BASE = config.get_val('smartthings', 'url_base', False, 'str')
+            config.SMARTTHINGS_APP_ID = config.get_val('smartthings', 'app_id', False, 'str')
+            config.SMARTTHINGS_EVENT_CODES = config.get_val('smartthings', 'event_codes', [], 'listint')
+            logger.debug('SMARTTHINGS Enabled - event codes: %s' % (",".join([str (i) for i in config.SMARTTHINGS_EVENT_CODES])))
+            events.register('statechange', sendStNotification, [], [])
+            events.register('stateinit', sendStNotification, [], [])
 
-def sendStNotification(eventType, type, parameters, code, event, message, defaultStatus):
-    # logger.debug('sendStNotification')
-    # logger.debug('eventType %s' % eventType)
-    # logger.debug('type %s' % type)
-    # logger.debug('parameters %s' % parameters)
-    # logger.debug('code %d' % code)
-    # logger.debug('event %s' % event)
-    # logger.debug('message %s' % message)
-    # logger.debug('defaultStatus %s' % defaultStatus)
-    smartthingsRequest(eventType, type, parameters, code, event, message, defaultStatus)
-
-@gen.coroutine
-def smartthingsRequest(eventType, type, parameters, code, event, message, defaultStatus):
-    http_client = AsyncHTTPClient()
-    url = 'garbage'
-    if type == 'zone':
-        url = config.SMARTTHINGS_URL_BASE + "/" + config.SMARTTHINGS_APP_ID + "/panel/" + str(code) + "/" + str(int(parameters)) + "?access_token=" + config.SMARTTHINGS_ACCESS_TOKEN
-    elif type == 'partition':
-        url = config.SMARTTHINGS_URL_BASE + "/" + config.SMARTTHINGS_APP_ID + "/panel/" + str(code) + "/" + str(int(parameters[0])) + "?access_token=" + config.SMARTTHINGS_ACCESS_TOKEN
-    else:
-        logger.debug('Smartthings unhandled type: ' + type)
-        return
-    # logger.debug('Smartthings will send %s request: %s' % (type, url))
-    try:
-        res = yield http_client.fetch(url, method='GET')
-    except:
-        logger.debug('Smartthings exception for url %s' % url)
-    # logger.debug('Smartthings result: %s' % res)
+    @gen.coroutine
+    def send_notification(self, event_type, type, parameters, code, event, message, default_status):
+        """Send smartthings notificiation"""
+        http_client = AsyncHTTPClient()
+        url = 'garbage'
+        if type == 'zone':
+            url = config.SMARTTHINGS_URL_BASE + "/" + config.SMARTTHINGS_APP_ID + "/panel/" + str(code) + "/" + str(int(parameters)) + "?access_token=" + config.SMARTTHINGS_ACCESS_TOKEN
+        elif type == 'partition':
+            url = config.SMARTTHINGS_URL_BASE + "/" + config.SMARTTHINGS_APP_ID + "/panel/" + str(code) + "/" + str(int(parameters[0])) + "?access_token=" + config.SMARTTHINGS_ACCESS_TOKEN
+        else:
+            logger.debug('Smartthings unhandled type: ' + type)
+            return
+        # logger.debug('Smartthings will send %s request: %s' % (type, url))
+        try:
+            res = yield http_client.fetch(url, method='GET')
+            logger.debug('Smartthings notification sent')
+        except:
+            logger.debug('Smartthings exception for url %s' % url)
+        # logger.debug('Smartthings result: %s' % res)
